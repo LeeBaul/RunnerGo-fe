90aa008e6ca49244ef8acb2b9ea52f6150584631
liuguanglei@apipost.cn <liuguanglei123>
2022-10-24 10:20:51 +0800

    fix: push code

--- src/pages/Machine/index.jsx
+++ src/pages/Machine/index.jsx
@@ -7,30 +7,35 @@ const Machine = () => {
     return (
         <div className="machine">
             <div className="left">
-                <p>并发模式</p>
-                <p>阶梯模式</p>
-                <p>错误率模式</p>
-                <p>响应时间模式</p>
-                <p>每秒请求数模式</p>
+                <div className="left-container">
+                    <p>并发模式</p>
+                    <p>阶梯模式</p>
+                    <p>错误率模式</p>
+                    <p>响应时间模式</p>
+                    <p>每秒请求数模式</p>
+                </div>
             </div>
             <div className="right">
+                <div className=""></div>
                 <div className="right-container">
-                    <Group value="A">
-                        <Radio value="A">
-                            <span>持续时长</span>
-                            <Input />
-                        </Radio>
-                        <Radio value="B" disabled>
-                            <span>轮次</span>
-                            <Input />
-                        </Radio>
-                    </Group>
-                    <div className="common-item">
-                        <span>并发数</span>
+                    <div className="right-item">
+                        <span>起始并发数</span>
                         <Input />
                     </div>
-                    <div className="common-item">
-                        <span>预热</span>
+                    <div className="right-item">
+                        <span>并发数步长</span>
+                        <Input />
+                    </div>
+                    <div className="right-item">
+                        <span>步长执行时长</span>
+                        <Input />
+                    </div>
+                    <div className="right-item">
+                        <span>稳定持续时长</span>
+                        <Input />
+                    </div>
+                    <div className="right-item" style={{ marginBottom: 0 }}>
+                        <span>最大并发数</span>
                         <Input />
                     </div>
                 </div>
