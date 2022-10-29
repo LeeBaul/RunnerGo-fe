e861eb4f84757b4395d73b8dcbf6d560468db86b
liuguanglei@apipost.cn <liuguanglei123>
2022-10-29 20:51:12 +0800

    fix: push code

--- src/modals/SceneConfig/index.jsx
+++ src/modals/SceneConfig/index.jsx
@@ -11,6 +11,7 @@ import PreviewFile from '../PreviewFile';
 import { useTranslation } from 'react-i18next';
 
 import { RD_FileURL } from '@config';
+import axios from 'axios';
 
 
 const SceneConfig = (props) => {
